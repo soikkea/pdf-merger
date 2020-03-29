175c9c65859f57444996d3d53b35df0d91fb70f4
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-03-29 13:27:29 +0300

    Add IsNotEmpty Converter

---
+++ PdfMerger/ViewModels/Converters.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace PdfMerger.ViewModels
+{
+    public static class Converters
+    {
+        public static bool IsNotEmpty(ICollection list) => list?.Count > 0;
+    }
+}
